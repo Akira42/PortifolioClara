4089280fed2f0784577c91a743452cd4c4fb4422
akira42 <vitorakira42@gmail.com>
2025-09-04 20:26:16 -0300

    development

--- src/react/footer/footer.js
+++ src/react/footer/footer.js
@@ -3,7 +3,7 @@ import './footer.scss';
 function Footer() {
   return (
     <footer className="footer-container">
-        <div className="https://portfolio-vitor-nagahara.vercel.app/">
+        <div className="footer-wrapper">
           <a href="https://portfolio-vitor-nagahara.vercel.app/" target="_blank" rel="nofollow">Codificado por <br/> Vitor Akira</a>
 
           <p>UX/UI E Design por <br/> Clara Guerreiro</p>
